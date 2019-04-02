43c422c07527b5e6ec8f961005cf2e85dfcb9afb
cshen <cshen@cam2cloud.com>
2019-04-02 10:10:57 +0800

    fix crash on release

--- ios/IJKMediaPlayer/IJKMediaPlayer/IJKFFMoviePlayerController.m
+++ ios/IJKMediaPlayer/IJKMediaPlayer/IJKFFMoviePlayerController.m
@@ -598,7 +598,8 @@ inline static int getPlayerOption(IJKFFOptionCategory category)
     __unused id weakHolder = (__bridge_transfer IJKWeakHolder*)ijkmp_set_inject_opaque(_mediaPlayer, NULL);
     __unused id weakijkHolder = (__bridge_transfer IJKWeakHolder*)ijkmp_set_ijkio_inject_opaque(_mediaPlayer, NULL);
     ijkmp_dec_ref_p(&_mediaPlayer);
-
+    _view = nil;
+    _glView = nil;
     [self didShutdown];
 }
 
